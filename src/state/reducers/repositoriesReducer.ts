36a49250fa82418f6b71392bab828c14147fc7dd
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-01 18:38:20 +0100

    wokring on redux and typescript

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,32 +1,10 @@
+import { ActionType } from '../action-type';
+import { Action } from '../actions';
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
-}
-
-interface SearchRepositoriesAction {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoritesErrorAction {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-  | SearchRepositoriesAction
-  | SearchRepositoriesSuccessAction
-  | SearchRepositoritesErrorAction;
-
-enum ActionType {
-  SEARCH_REPOSITORIES = 'search_repositories',
-  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
 }
 
 // interface Action {
